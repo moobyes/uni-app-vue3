82cf0ac283e8ecb2a20d9d82e1969ab32d4471ce
Moobye <moobyes@gmail.com>
2021-09-30 21:57:16 +0800

    feat: 完成基础设置和代码示例

--- src/utils/request.js
+++ src/utils/request.js
@@ -2,11 +2,13 @@
 import signVerify from './signVerify';
 import { showError } from './utils';
 
-const baseUrl = process.env.NODE_ENV === 'development' ? 'https://gateway3.test.m3.m3cube.cn/recv' : 'https://gateway-bar.cubem3.cn/recv';
+const baseUrl =
+  process.env.NODE_ENV === 'development'
+    ? 'https://gateway3.test.m3.m3cube.cn/recv'
+    : 'https://gateway-bar.cubem3.cn/recv';
 
-function baseRequest ({method = 'GET', data, formData, }) {
+function baseRequest({ method = 'GET', data, url = baseUrl, formData }) {
   return new Promise((resolve, reject) => {
-
     // 没有 token，并且不在白名单，不允许请求
     // if (!uni.getStorageSync('uticket')) {
     //   uni.navigateTo({ url: '/pages/public/login' })
@@ -34,19 +36,21 @@ function baseRequest ({method = 'GET', data, formData, }) {
       headers['Content-type'] = 'application/x-www-form-urlencoded';
     }
     // 发起请求
-    // eslint-disable-next-line no-undef
     uni.request({
-      url: baseUrl, // 接口url是固定的，通过service区分不同的接口
-      data: {...data, ..._signVerify, },
+      url, // 接口url是固定的，通过service区分不同的接口
+      data: { ...data, ..._signVerify },
       method,
       header,
       success: (response) => {
         uni.hideLoading();
 
-        const res = response.data;
+        const { state, data, errMsg } = response;
 
-        resolve(res);
-        // uni.navigateTo({ url: '/pages/public/Login' })
+        if (+state === 200) {
+          resolve(data);
+        } else {
+          showError(errMsg || '服务器错误，稍后再试');
+        }
       },
       fail: (e) => {
         uni.hideLoading();
@@ -69,30 +73,39 @@ function baseRequest ({method = 'GET', data, formData, }) {
  * @param opts
  * @returns {Promise}
  */
-const get = async (opts) => baseRequest({method: 'GET', ...opts, });
+const get = async (opts) => baseRequest({ method: 'GET', ...opts });
 
 /**
  * post
  * @param opts
  * @returns {Promise}
  */
-const post = async (opts) => baseRequest({
-  method: 'POST',
-  ...opts,
-});
+const post = async (opts) =>
+  baseRequest({
+    method: 'POST',
+    ...opts,
+  });
 
 /**
  * put
  * @param opts
  * @returns {Promise}
  */
-const put = async (opts) => baseRequest({
-  method: 'POST',
-  ...opts,
-});
+const put = async (opts) =>
+  baseRequest({
+    method: 'POST',
+    ...opts,
+  });
 
-export {
-  get,
-  post,
-  put
-};
+/**
+ * delete
+ * @param opts
+ * @returns {Promise}
+ */
+const del = async (url, data = '') =>
+  baseRequest({
+    method: 'DELETE',
+    ...opts,
+  });
+
+export { get, post, put, del };
